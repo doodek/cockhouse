4578a734e079fe2167653fb4feef65fcb9a5716a
Kacper Malina <kacmalina@gmail.com>
2022-04-24 16:58:45 +0200

    Offset reading from DIPs

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define read1(r, o) ((r >> o) & 1)
+
+#define CONFIG_PUB ((1<<PB0) | (1<<PB2) | (1<<PB3) | (1<<PB4) | (1<<PB5))
+#define CONFIG_PUD ((1<<PD7) | (1<<PD6) | (1<<PD5))
+
 // basis for start and end of the door open window
 // (hour<<8)|mins
 #define BASIS_START 0x0600
 #define BASIS_END 0x2000
 
 void debug() {
-  PORTB &= ~(1<<PB0);
-  _delay_ms(200);
-  PORTB |= (1<<PB0);
-  _delay_ms(200);
+  //  PORTB &= ~(1<<PB0);
+  //  _delay_ms(200);
+  //  PORTB |= (1<<PB0);
+  //  _delay_ms(200);
 }
 
 #include "i2c.c"
@@ -43,30 +48,34 @@ int main(){
 
   // watchdog
 
-
-  DDRB |= (1<<PB0); // LED debug blinken
+  
   DDRC |= (1<<PC5) | (1<<PC4); // I2C RTC
-  // DDRD |= (1<<PD1); // USART
 
-  PORTB |= (1<<PB0); // LED
+  // Setup Input Pullup on Config DIPs
+  PORTB |= CONFIG_PUB;
+  PORTD |= CONFIG_PUD;
+
+  // save offsets in minutes, [-105, 105]
+  const static signed char time_start_offset =
+    (read1(PINB, PB0) != 0) ? -1 : 1) *
+    (read1(PIND, PD7) * 15 + read1(PIND, PD6) * 30 + read1(PIND, PD5) * 60));
+
+  const static signed char time_end_offset =
+    (read1(PINB, PB2) != 0) ? -1 : 1) *
+    (read1(PINB, PB3) * 15 + read1(PINB, PB4) * 30 + read1(PINB, PB5) * 60));
+
+  // Remove Pullup on Config (Hi-Z)
+  PORTB &= ~CONFIG_PUB;
+  PORTD &= ~CONFIG_PUD;
 
   TCCR1B |= (1 << CS12) | (1 << CS10); // prescaler 1024
   TIMSK |= (1 << TOIE1); // enable overflow interrupt
 
   _delay_ms(300);
 
-  // UCSRB |= (1<<RXEN) | (1<<RXCIE) | (1<<TXEN); // USART
-  // UCSRC |= (1<<URSEL) | (1<<UCSZ1) | (1<<UCSZ0);  //Packet 8b
-
   sei();
 
-  while(1) {
-    // readRegisters(registers);   
-
-    // PORTB ^= (1<<PB0);
-    // _delay_ms(500);
-  }
-
+  while(1) {}
 }
 
 ISR(TIMER1_OVF_vect) {
